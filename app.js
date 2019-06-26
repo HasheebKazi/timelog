f4da0b543380a1e27dfc9eb1fbe119efedf0e919
HasheebKazi <hasheeb.k@gmail.com>
2019-06-26 10:53:41 -0400

    first view

--- app.js
+++ app.js
@@ -2,10 +2,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+// node core libraries
+const path = require('path');
 
+// initialize and configure app
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+
+// request response cycle
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
     res.render('index');
