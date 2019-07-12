e2e31fb1cf8b199501fbcf94a41de5724fe57207
HasheebKazi <hasheeb.k@gmail.com>
2019-07-11 21:06:05 -0400

    work on adding in sessions

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const express = require('express');
 // const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
 const flash = require('connect-flash');
 
@@ -11,7 +13,13 @@ const fs = require('fs');
 
 // const MONGODB_URI = process.env.MONGODB_URI;
 const MONGODB_URI = 'mongodb+srv://node_project:AkWcOzQBkbWJdXoA@summer-2019-fo8l7.mongodb.net/timelog';
-console.log(MONGODB_URI);
+const store = new MongoDBStore({
+    uri: MONGODB_URI,
+    collection: 'sessions'
+});
+const csrfProtection = csrf();
+
+
 
 // internal imports
 // controllers
@@ -19,6 +27,17 @@ const loggerRoutes = require('./routes/logger_routes');
 
 // initialize and configure app
 const app = express();
+app.use(
+    session({
+        secret: 'my secret',
+        resave: false,
+        saveUninitialized: false,
+        store: store
+    })
+);
+app.use(csrfProtection);
+app.use(flash());
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
